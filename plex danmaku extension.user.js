5da3f4fa3992f58ccfec49637d28e0c9e5482a4f
lsl330 <54297603+lsl330@users.noreply.github.com>
2022-04-12 10:36:24 +0800

    Update plex danmaku extension.user.js

--- plex danmaku extension.user.js
+++ plex danmaku extension.user.js
@@ -96,6 +96,12 @@ function actionFunction (is_init=true) {
     danmaku=null
     episode_info=null
     selecAnime_id=0
+	
+    var sleep = function(time) {
+    var startTime = new Date().getTime() + parseInt(time, 10);
+    while(new Date().getTime() < startTime) {}
+};
+    sleep(200); // 延时函数，单位ms
 
     if(document.getElementById('searchDanmaku') == undefined){
         initButton()
